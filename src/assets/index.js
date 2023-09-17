49e0e2bdda6f8436770d4b980cdcadfe08caed76
nova vita <42131370+josephc1417@users.noreply.github.com>
2023-09-17 00:55:54 -0400

    File extensions in the Asset's folder need to be updated

--- src/assets/index.js
+++ src/assets/index.js
@@ -29,8 +29,10 @@ import tesla from "./company/tesla.png";
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
+import JC from "./JC.svg"
 
 export {
+  JC,
   logo,
   backend,
   creator,
--- src/styles.js
+++ src/styles.js
@@ -1,6 +1,6 @@
 //Tailwind Utility Styles
 
-const styles = {
+ export const styles = {
     paddingX: "sm:px-16 px-6",
     paddingY: "sm:py-16 py-6",
     padding: "sm:px-16 px-6 sm:py-16 py-10",
