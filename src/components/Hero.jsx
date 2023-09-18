53e6741d218db93497a55edbf0e99c1bee681bab
nova vita <42131370+josephc1417@users.noreply.github.com>
2023-09-17 21:19:49 -0400

    Updated Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'></span>
+            Hi, I'm <span className='text-[#915EFF]'>Joseph</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I Develop Full Stack Web Applications, and <br className='sm:block hidden' />
